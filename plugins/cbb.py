b66db176b41d82ba3929bd13907e1feb5ff40730
MrC《》VENOM <68112220+nandhu663@users.noreply.github.com>
2021-09-07 11:57:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Master : <a href='https://t.me/COLD_ONEz'>COLD_ONEz</a>\n\n○ Master : <a href='https://t.me/mrk_yt'>MRK_YT</a>\n\n○ Group : <a href='https://t.me/Mo_Tech_Group'>Mo_Tech_Group</a>\n\n○ Channel : <a href='https://t.me/Mo_Tech_YT'>Mo_Tech_YT</a></b>",
+            text = f"<b>○ Master : <a href='https://t.me/MrC_VENOM'>MrC《》VENOM</a>\n\n○ Group : <a href='https://t.me/tvseriezzz'>♠️ 𝑨𝒍𝒍 𝑰𝒏 𝑶𝒏𝒆 𝑮𝒓𝒐𝒖𝒑 🎬</a>\n\n○ Channel : <a href='https://t.me/tvseriezzz_update'>♠️ 𝑨𝒍𝒍 𝑰𝒏 𝑶𝒏𝒆 𝑮𝒓𝒐𝒖𝒑 {𝐔𝐩𝐝𝐚𝐭𝐞}</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
